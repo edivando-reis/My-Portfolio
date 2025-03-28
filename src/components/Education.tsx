5b2f0439972615e7f08aa03f991fb239d63094d3
DidoReis <djdidoreis@gmail.com>
2025-03-28 10:15:48 -0300

    change language education

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -224,7 +224,7 @@ const Education: React.FC = () => {
         {/* Cursos Complementares */}
         <div className="mt-12">
           <h3 className="text-xl font-bold mb-6 text-primary">
-            Cursos complementares.
+          {t('complementary.courses')}
           </h3>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {courseData.map((item, index) => (
